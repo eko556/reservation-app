b682043f02e09b87e0da385338b02ae0eed70e12
eko556 <arink.kerio@gmail.com>
2024-11-27 22:44:52 +0900

    komipushi

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -3,6 +3,7 @@ import { RouterLink } from '@angular/router';
 import { RouterLinkActive } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { NgFor } from '@angular/common';
+import { products } from '../../products';
 @Component({
   selector: 'app-product-list',
   imports: [RouterLink, RouterLinkActive, RouterOutlet, NgFor],
@@ -10,5 +11,5 @@ import { NgFor } from '@angular/common';
   styleUrl: './product-list.component.scss'
 })
 export class ProductListComponent {
-  products: any = [1,2,3,4];
+  products = [...products];
 }
---
+++ src/app/products.ts
@@ -0,0 +1,43 @@
+export type Product = {
+  name: string
+  price: number
+  description: string
+  heading1: string
+  heading2: string
+  heading3: string
+};
+export const products: Product[] = [
+{
+  name: 'Phone XL',
+  price: 799,
+  description: 'A large phone with one of the best screens',
+  heading1: 'PhoneXLのheading1です',
+  heading2: 'PhoneXLのheading2です',
+  heading3: 'PhoneXLのheading3です',
+},
+{
+  name: 'Phone Mini',
+  price: 699,
+  description: 'A great phone with one of the best cameras',
+  heading1: 'PhoneMiniのheading1です',
+  heading2: 'PhoneMiniのheading2です',
+  heading3: 'PhoneMiniのheading3です',
+},
+{
+  name: 'Phone Standard',
+  price: 299,
+  description: '',
+  heading1: 'PhoneStandardのheading1です',
+  heading2: 'PhoneStandardのheading2です',
+  heading3: 'PhoneStandardのheading3です',
+},
+{
+  name: 'Phone Special',
+  price: 999,
+  description: '',
+  heading1: 'PhoneSpecialのheading1です',
+  heading2: 'PhoneSpecialのheading2です',
+  heading3: 'PhoneSpecialのheading3です',
+},
+
+]
