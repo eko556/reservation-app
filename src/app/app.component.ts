e3a7cfb03fbeee841e742979d33ec9b5d9b7d3a3
eko556 <arink.kerio@gmail.com>
2024-12-02 22:07:43 +0900

    serviceの使い方がよくわからん

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './common/navbar/navbar.component';
 import { ProductComponent } from './product/product.component';
+import { ProductService } from './product/shared/products.service';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, NavbarComponent, ProductComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NavbarComponent, ProductComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
