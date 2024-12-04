43a9e4fa9b06d412a5c8ddaa2e9db04d40dc2acc
eko556 <arink.kerio@gmail.com>
2024-12-04 22:43:33 +0900

    deploy

--- server/index.js
+++ server/index.js
@@ -1,30 +1,44 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const config = require('./config/dev');
-const FakeDb = require('./fake-db');
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("./config/");
+const FakeDb = require("./fake-db");
 
-const productRoutes = require('./routes/products');
+const productRoutes = require("./routes/products");
+const path = require("path");
 
-mongoose.connect(config.DB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(
-  () => {
+mongoose
+  .connect(config.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     const fakeDb = new FakeDb();
     fakeDb.initDb();
-  }
-)
+  });
 
-const app = express()
-app.use('/api/v1/products', productRoutes)
+const app = express();
+app.use("/api/v1/products", productRoutes);
 
-app.get('/products', function(req, res) {
-  res.json({'success': true})
-})
+if (process.env.NODE_ENV === "production") {
+  const appPath = path.join(
+    __dirname,
+    "..",
+    "dist",
+    "reservation-app",
+    "browser"
+  );
+  app.use(express.static(appPath));
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(appPath, "index.html"));
+  });
+}
 
-const PORT = process.env.PORT || '3001';
+app.get("/products", function (req, res) {
+  res.json({ success: true });
+});
 
-app.listen('3001', function(){
-  console.log('I am running!');
-})
+const PORT = process.env.PORT || "3001";
 
+app.listen("3001", function () {
+  console.log("I am running!");
+});
